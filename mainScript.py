4d4b9f1e1e7427ea93746179a2d09946bc7e3e97
Rishikesh-Hole <rishikeshhole@live.com>
2023-04-29 19:04:41 +0530

    New changes for testing

--- mainScript.py
+++ mainScript.py
@@ -1,7 +1,16 @@
 import sys
+
 print("Welcome to python project..!!")
 if 1 > 0:
-    print ("Hello you are in if case!!")
+    print("Hello you are in if case!!")
 
-name =input(" Please enter your name - ")
-print(f"You have enetered - {name}")
+# name =input(" Please enter your name - ")
+# print(f"You have enetered - {name}")
+name ="Rishikesh Hole"
+print(f"My name is {name}")
+
+name="Rishikesh Holey"
+print(f"My name is {name}")
+
+name = "RIshikesh Digamber Hole"
+print(f"My name is {name}")
