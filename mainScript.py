aa5952426f29aab417d8c7ec4e7c6d2d4467317d
Rishikesh-Hole <rishikeshhole@live.com>
2023-04-29 19:00:18 +0530

    first commit

---
+++ mainScript.py
@@ -0,0 +1,7 @@
+import sys
+print("Welcome to python project..!!")
+if 1 > 0:
+    print ("Hello you are in if case!!")
+
+name =input(" Please enter your name - ")
+print(f"You have enetered - {name}")
